293ddd09cc8bb110a65c543d7507ed61998e0526
vagrant <noura4403@gmail.com>
2022-01-03 20:54:50 +0000

    working on more queries

---
+++ 4_queries/10_most_confusing_assignments.sql
@@ -0,0 +1,6 @@
+SELECT 
+assignments.id as id, day, chapter, name, count(*) as total_requests 
+FROM assignments
+JOIN assistance_requests ON assignments.id = assignment_id
+GROUP BY assignments.id
+ORDER BY total_requests DESC;
